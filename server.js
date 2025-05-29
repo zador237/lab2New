0546edfca8e65a2fa4d65351a7e70b111c4df20e
zador237 <chejosephfuh@gmail.com>
2025-05-29 15:00:19 -0400

    404 resolved.2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use (express.urlencoded ({ extended: true }));
 
 
 
-app.get ('./', (req, res) => {
+app.get ('/', (req, res) => {
     res.json({"message": "API Listening",
         "term": "Summer 2025", 
         "student": "Che Joseph Fuh",
